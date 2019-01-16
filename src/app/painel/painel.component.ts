47fccce78f91de693084c0c821a4fa15415a1f2c
Arthur Barbosa <arthurbruno03@gmail.com>
2019-01-16 16:44:06 -0300

    comparando a frase digitada

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,10 +12,38 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = 'Traduza a frase:';
   public frases: Frase[] = FRASES;
+  public resposta: string;
 
-  constructor() { console.log(this.frases) }
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)
+   }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    //console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert("Tradução correta.")
+     
+      //atualiza rodada
+      this.rodada++
+
+      //atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada]
+      console.log(this.rodada)
+    }else{
+      alert("Tradução incorreta.")
+    }
+   
   }
 
 }
